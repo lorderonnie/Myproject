319c38f4a98249fe2631e4eab8a086bdd329cde7
lorderonnie <rontheking45@gmail.com>
2020-01-12 00:25:20 +0300

    created a url link to my profile and update profile

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home,name='home'),
-    
+    path('project/profile/',views.profile,name="profile"),
+    url(r'^updateprofile/$',views.updateprofile,name='updateprofile'),
 ]
